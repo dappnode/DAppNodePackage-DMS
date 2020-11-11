03821b239a1448a0f63c20f47633b7cde1e273f6
dapplion <guillem.lion.benosa@gmail.com>
2020-11-11 21:12:33 +0000

    Configure prometheus sd

--- manager/src/params.ts
+++ manager/src/params.ts
@@ -1,5 +1,5 @@
 export const GRAFANA_API_URL = "http://grafana:80";
-export const PROMETHEUS_TARGETS_DIR = "/provisioning";
+export const PROMETHEUS_TARGETS_DIR = "/prometheus_file_sd";
 export const DAPPMANAGER_API_URL = "http://my.dappnode";
 export const JSON_DB_PATH = "/data/db.json";
 
