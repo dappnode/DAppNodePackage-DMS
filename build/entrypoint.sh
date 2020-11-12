d5a231d4eabff81da5ec2af3aa21b6659d23e66f
dapplion <guillem.lion.benosa@gmail.com>
2020-11-12 10:15:25 +0000

    Remove legacy build directory

--- build/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp /etc/grafana/provisioning/dashboards/*.json /provisioning/dashboards/
-cp /etc/prometheus/targets/*.json /provisioning/targets/
-
-exec supervisord -c /etc/supervisord/supervisord.conf
